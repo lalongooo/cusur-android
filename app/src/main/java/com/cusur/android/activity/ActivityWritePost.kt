de90eabf37186a943c13d6bfe38d9ffb82c521d6
Jorge E. Hernández <hdez.jeduardo@gmail.com>
2017-10-05 23:59:20 -0500

    Get the full picture after the picture is taken

--- app/src/main/java/com/cusur/android/activity/ActivityWritePost.kt
+++ app/src/main/java/com/cusur/android/activity/ActivityWritePost.kt
@@ -3,8 +3,10 @@ package com.cusur.android.activity
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 import android.text.TextUtils
 import android.view.View
 import android.widget.Toast
@@ -14,14 +16,19 @@ import com.cusur.android.dataclass.Publication
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_take_picture.*
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
+
 
 class ActivityWritePost : BaseActivity() {
 
-    private val REQUEST_IMAGE_CAPTURE = 567
+    private val REQUEST_TAKE_PHOTO = 567
     private val databaseReference = mDatabase.getReference("publication")
     private val userId = mFirebaseUser?.uid ?: ""
     lateinit var byteArrayPicture: ByteArray
+    lateinit var mCurrentPhotoPath: String
 
     companion object {
         val EXTRA_TAKE_PICTURE = "take_picture"
@@ -35,15 +42,36 @@ class ActivityWritePost : BaseActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            val extras = data?.extras
-            val bitmap = extras?.get("data") as Bitmap?
-            if (bitmap != null) {
-                handleTakenPicture(bitmap)
-            }
-        } else if (requestCode == REQUEST_IMAGE_CAPTURE) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
+            setPic()
+        } else if (requestCode == REQUEST_TAKE_PHOTO) {
             finish()
         }
+    }
+
+
+    private fun setPic() {
+        // Get the dimensions of the View
+        val targetW = ivTakenPicture.width
+        val targetH = ivTakenPicture.height
+
+        // Get the dimensions of the bitmap
+        val bmOptions = BitmapFactory.Options()
+        bmOptions.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions)
+        val photoW = bmOptions.outWidth
+        val photoH = bmOptions.outHeight
+
+        // Determine how much to scale down the image
+        val scaleFactor = Math.min(photoW / targetW, photoH / targetH)
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false
+        bmOptions.inSampleSize = scaleFactor
+        bmOptions.inPurgeable = true
+
+        val bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions)
+        ivTakenPicture.setImageBitmap(bitmap)
     }
 
     private fun handleExtras(intent: Intent) {
@@ -56,22 +84,78 @@ class ActivityWritePost : BaseActivity() {
         }
     }
 
+//    private fun dispatchTakePictureIntent() {
+//        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        if (takePictureIntent.resolveActivity(packageManager) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+//        }
+//    }
+
+
     private fun dispatchTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (takePictureIntent.resolveActivity(packageManager) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        // Ensure that there's a camera activity to handle the intent
+//        if (takePictureIntent.resolveActivity(packageManager) != null) {
+//            // Create the File where the photo should go
+//            try {
+//                val photoFile = createImageFile()
+//                val photoURI = FileProvider.getUriForFile(
+//                        this@ActivityWritePost,
+//                        "com.cusur.fileprovider",
+//                        photoFile)
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+//                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+//            } catch (ex: IOException) {
+//                // Error occurred while creating the File
+//                Log.d("JEH", "An error occurred while trying to create the file")
+//            }
+//        }
+
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            var photoFile: File? = null
+            try {
+                photoFile = createImageFile();
+            } catch (ex: IOException) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                val photoURI = FileProvider.getUriForFile(
+                        this@ActivityWritePost,
+                        "com.cusur.fileprovider",
+                        photoFile
+                )
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
+
+
+    }
+
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val imageName = "PIC_$timeStamp"
+        val imagePath = File(filesDir, "images")
+        imagePath.mkdirs()
+        val imageFile = File.createTempFile(imageName, ".jpg", imagePath)
+        mCurrentPhotoPath = imageFile.absolutePath
+        return imageFile
     }
 
     private fun handleTakenPicture(takenPicture: Bitmap) {
-        ivTakenPicture.setImageBitmap(takenPicture)
         compressPicture(takenPicture)
+        ivTakenPicture.setImageBitmap(takenPicture)
     }
 
     private fun compressPicture(takenPicture: Bitmap) {
 
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        takenPicture.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        val byteArrayOutputStream = ByteArrayOutputStream(takenPicture.width * takenPicture.height)
+        takenPicture.compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream)
         byteArrayPicture = byteArrayOutputStream.toByteArray()
 
     }
@@ -86,7 +170,7 @@ class ActivityWritePost : BaseActivity() {
                 btnPost.isEnabled = false
 
                 val picturesFolder = "pictures/"
-                val fileName = "/" + UUID.randomUUID().toString() + ".png"
+                val fileName = "/" + UUID.randomUUID().toString() + ".jpg"
                 val storageReference: StorageReference = mStorage.getReference(picturesFolder + userId + fileName)
                 val uploadTask = storageReference.putBytes(byteArrayPicture)
 
